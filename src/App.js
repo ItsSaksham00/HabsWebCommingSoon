2a14824fe91b7505456a42396fdf774cac6500f4
ItsSaksham00 <155241009+ItsSaksham00@users.noreply.github.com>
2023-12-30 20:11:28 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import Lottie, {LottieRefCurrentProps} from 'lottie-react';
+import underdevData from './UnderDevelopment.json';
+import './App.css';
+import { useRef } from 'react';
+const App = () => {
+
+  const underDevRef = useRef<LottieRefCurrentProps>(null);
+
+  return(
+    <>
+
+      <div className="UnderConstruction">
+          <div className="lottieFile">
+
+          <Lottie className='LottieAni' animationData={underdevData}
+                        lottieRef={underDevRef}  
+          
+            />
+
+          </div>
+          <div className="Detail">
+              <h2>Seems like the web is Under Construction </h2>
+              <a href="https://sakshamk.com.np/?i=1" target='_blank'>-by Saksham</a>
+          </div>
+      </div>
+
+      <footer>
+        <p>All rights reserved © Hamro Awaj Baal Sanjal</p>
+      </footer>
+
+    </>
+
+  );
+}
+
+export default App;
